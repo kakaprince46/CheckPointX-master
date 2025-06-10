650dada0ea63d3e6d3854945159d9f02151dea8b
kakaprince46 <kakaprince46@gmail.com>
2025-06-10 07:24:43 +0100

    Initial commit

--- render_build.sh
+++ render_build.sh
@@ -3,23 +3,18 @@
 set -o errexit
 
 echo "INFO [render_build.sh]: Starting build process..."
-
-echo "INFO [render_build.sh]: Upgrading pip..."
 pip install --upgrade pip
-
-echo "INFO [render_build.sh]: Installing dependencies from requirements.txt..."
 pip install -r requirements.txt
 
 # Set FLASK_APP and FLASK_CONFIG for the migration command
 export FLASK_APP="app:create_app"
 export FLASK_CONFIG="prod"
 
-echo "INFO [render_build.sh]: FLASK_APP is ${FLASK_APP}"
 echo "INFO [render_build.sh]: FLASK_CONFIG is ${FLASK_CONFIG}"
+echo "INFO [render_build.sh]: About to run database migrations (flask db upgrade)..."
 
-# This command applies migrations to the database specified in ProductionConfig
-# which is pulled from the DATABASE_URL environment variable
-echo "INFO [render_build.sh]: Running database migrations (flask db upgrade)..."
+# This command applies migrations to the database specified in ProductionConfig,
+# which is pulled from the DATABASE_URL environment variable.
 flask db upgrade
 
 echo "INFO [render_build.sh]: Database migrations complete."
